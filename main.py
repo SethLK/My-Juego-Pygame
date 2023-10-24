2dfe7d755de169117b594b65b4bbc53d58d87321
SethLK <sethlk2006@gmail.com>
2023-10-24 10:18:48 +0630

    Add score bar and random size

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import sys
+
 pygame.init()
 
 screen_width = 600
@@ -10,22 +11,27 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Click the Colors")
 
-icon = pygame.image.load("icon.png")  # Replace "icon.png" with the path to your icon image.
+icon = pygame.image.load("icon.png")
 pygame.display.set_icon(icon)
 
+# Create a font for the score display
+font = pygame.font.Font(None, 36)
 
 def random_color():
     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 def draw_random_circle():
-    radius = 30
+    max_radius = 60  # Maximum radius
+    radius = random.randint(30, max_radius)
     x = random.randint(radius, screen_width - radius)
     y = random.randint(radius, screen_height - radius)
     color = random_color()
     return (x, y, radius, color)
 
 circles = []
+score = 0  # Initialize the score
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -37,6 +43,7 @@ while running:
 
                 if pygame.Rect(x - radius, y - radius, 2 * radius, 2 * radius).collidepoint(event.pos):
                     circles.remove(circle)
+                    score += 1  # Increase the score when a circle is clicked
 
     if len(circles) < 5:
         circles.append(draw_random_circle())
@@ -46,6 +53,10 @@ while running:
     for circle in circles:
         x, y, radius, color = circle
         pygame.draw.circle(screen, color, (x, y), radius)
+
+    # Display the score in the upper left corner
+    score_text = font.render(f"Score: {score}", True, (0, 0, 0))
+    screen.blit(score_text, (10, 10))
 
     pygame.display.flip()
 
